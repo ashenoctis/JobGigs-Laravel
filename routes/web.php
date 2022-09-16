30aa342e9e0859e7d5c4b4b3e612be92d3653544
Ashish Sharma <me@ashisharma.info>
2022-09-16 11:39:22 +0530

    Routing - Web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return response('<h1>Ashish Sharma</h1>', 200) //404, 500
+        ->header('Content-Type', 'text/plain') //text/html
+        ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    dd($id); //die and dump
+    ddd($id); //die, dump and die
+    return response('<h1>Post '.$id.'</h1>', 200);
+})->where('id', '[0-9]+'); //only numbers
+
+Route::get('/search', function (Request $request) {
+    dd($request->name . ' ' . $request->city); //name=Ashish&city=Bangalore
+});
+
