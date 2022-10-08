f1cfc77add062b5bcdc1e9891207e0899113b93d
Ashish Sharma <me@ashisharma.info>
2022-10-08 22:04:46 +0530

    Component Attributes

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,6 +1,6 @@
 @props(['listing']) <!-- pass props to the component -->
 
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+<x-card>
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
@@ -39,4 +39,4 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
