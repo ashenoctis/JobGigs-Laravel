8f4df6296b10a5ef5d4add66946730874135b22d
Ashish Sharma <me@ashisharma.info>
2022-10-08 22:20:18 +0530

    Component Tags

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
 <div {{$attributes->merge(['class' => 'bg-gray-50 border border-gray-200 rounded p-6'])}}
-    class="bg-gray-50 border border-gray-200 rounded p-6"> <!-- merge the attributes passed in the class -->
+> <!-- merge the attributes passed in the class -->
     {{$slot}} <!-- this is where the content of the component will be rendered -->
 </div>
